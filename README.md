61dd9d3c13d65f7fe739386dd3926377f8203afc
Evan Frohlich <evan.frohlich@controlgroup.com>
2012-11-21 17:33:23 -0500

    Added optional render types and clean view for full render mode

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Yii Extension: Quickly Add Dynamic Content Without Writing JS Or Additional Acti
 
 **Example Usage**
 ```php
-class TestController extends Controller
+class AsteroidController extends Controller
 	{
 		public function behaviors() {
 			return array('EAsteroid' => array('class'=>'ext.Asteroid.behaviors.EAsteroid'));
@@ -14,7 +14,7 @@ class TestController extends Controller
 
 		public function actionTestUI()
 		{
-			$this->Asteroid('part1')->append('#myPlace', '_myPlace1',   function() { return array('dude' =>'yeah!' ); } )
+			$this->Asteroid('part1')->renderMethod('render')->append('#myPlace', '_myPlace1',   function() { return array('model' =>new Work() ); } )
 					 ->Asteroid('part2')->prepend('#myPlace2', '_myPlace2', function() { return array('dude2'=>'yeah2!'); } )
 					 ->Asteroid('part3')->replace('#myPlace3', '_myPlace3', function() { return array('dude3'=>'yeah3!'); } )
 					 ->orbit();
@@ -22,5 +22,4 @@ class TestController extends Controller
 			$this->render('test');
 		}	
 		
-	}
-```
+	}```
--- behaviors/EAsteroid.php
+++ behaviors/EAsteroid.php
@@ -1,19 +1,19 @@
 <?php
 	class EAsteroid extends CBehavior 
 	{
-		public $AsteroidActionID;
-		public $comets = array();
-		public $AsteroidID;
-		public $AsteroidCometRender = 'partial';
+		public $_AsteroidActionID;
+		public $_comets = array();
+		public $_AsteroidID;
+		public $_AsteroidCometRender = 'renderPartial';
 		public $_assetsUrl = null;
 
 		public function Asteroid($id)
 		{
-			if(!isset($this->AsteroidActionID) && isset($_GET['AsteroidActionID']))
-				$this->AsteroidActionID = $_GET['AsteroidActionID'];
+			if(!isset($this->_AsteroidActionID) && isset($_GET['AsteroidActionID']))
+				$this->_AsteroidActionID = $_GET['AsteroidActionID'];
 
-			$this->AsteroidID = $id;
-			$this->comets[$id] = array();
+			$this->_AsteroidID = $id;
+			$this->_comets[$id] = array();
 			
 			//Register Scripts If we are not rendering a comet
 			if(!isset($_GET['AsteroidActionID']))
@@ -29,10 +29,10 @@
 
 		public function orbit()
 		{
-			if(!isset($_GET['AsteroidActionID']) && !empty($this->comets))
+			if(!isset($_GET['AsteroidActionID']) && !empty($this->_comets))
 			{
 				$comets = array();
-				foreach($this->comets as $id=>$config)
+				foreach($this->_comets as $id=>$config)
 					$comets[] = array('id'=>$id, 'renderType'=>$config->renderType, 'element'=>$config->element);
 		
 				Yii::app()->clientScript->registerScript('script', 'var asteroidConfig = ' . CJSON::encode($comets), CClientScript::POS_HEAD);
@@ -40,43 +40,48 @@
 		}
 		
 
-		public function renderAs($type='partial')
+		public function renderMethod($type='renderPartial')
 		{
-			$this->AsteroidCometRender = 'render';
+			$this->_AsteroidCometRender = 'render';
 			return $this;
 		}
 
 		public function append($element, $template, $data)
 		{
-			$this->setComet($this->AsteroidID, array('renderType'=>'append', 'element'=>$element, 'template'=>$template, 'data'=>$data));
+			$this->setComet($this->_AsteroidID, array('renderType'=>'append', 'element'=>$element, 'template'=>$template, 'data'=>$data));
 			return $this;
 		}
 
 		public function prepend($element, $template, $data)
 		{
-			$this->setComet($this->AsteroidID, array('renderType'=>'prepend', 'element'=>$element, 'template'=>$template, 'data'=>$data));
+			$this->setComet($this->_AsteroidID, array('renderType'=>'prepend', 'element'=>$element, 'template'=>$template, 'data'=>$data));
 			return $this;
 		}
 
 		public function replace($element, $template, $data)
 		{
-			$this->setComet($this->AsteroidID, array('renderType'=>'replace', 'element'=>$element, 'template'=>$template, 'data'=>$data));
+			$this->setComet($this->_AsteroidID, array('renderType'=>'replace', 'element'=>$element, 'template'=>$template, 'data'=>$data));
 			return $this;
 		}
 
 		public function setComet($id, $config = array())
 		{
-			$this->comets[$id] = (object) $config;
-			if($this->AsteroidActionID && $this->AsteroidActionID == $id)
+			$this->_comets[$id] = (object) $config;
+			if($this->_AsteroidActionID && $this->_AsteroidActionID == $id)
 				$this->execComet($id);
 			return true;
 		}
 
 		public function execComet($id)
 		{
-			$comet = $this->comets[$id];
+			$comet = $this->_comets[$id];
 			$data = $comet->data;
-			$this->owner->renderPartial($comet->template, $data());
+			if($this->_AsteroidCometRender == 'render')
+			{
+				$this->owner->layout = 'ext.Asteroid.views.clean';
+			}
+			
+			$this->owner->{$this->_AsteroidCometRender}($comet->template, $data());
 			Yii::app()->end();
 		}
 
