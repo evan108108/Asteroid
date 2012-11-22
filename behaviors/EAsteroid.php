9f5fa5b381c5fb1a4a0f0adaa9a70c28e886fb9c
Evan Frohlich <evan.frohlich@controlgroup.com>
2012-11-22 12:25:21 -0500

    Added comments and cleanup

--- behaviors/EAsteroid.php
+++ behaviors/EAsteroid.php
@@ -1,12 +1,15 @@
 <?php
 	class EAsteroid extends CBehavior 
 	{
-		public $_AsteroidActionID;
-		public $_comets = array();
-		public $_AsteroidID;
-		public $_AsteroidCometRender = 'renderPartial';
-		public $_assetsUrl = null;
+		public $_AsteroidID; //The unique idenifier of a section of dynamically rendered content
+		public $_AsteroidActionID; //Set When $_GET['AsteroidActionID'] is sent. Tells EAsteroid which commet to render. 
+		public $_comets = array(); //List of all content sections to be rendered.
+		
+		public $_AsteroidCometRender = 'renderPartial'; //Tells Yii how content should be rendered.
+		public $_assetsUrl = null; //Path to Asteroid Assets.
 
+		//Initilize Asteroid and create a comet for the $id passed
+		//$id should be unique unless you intend to overwrite an existing comet.
 		public function Asteroid($id)
 		{
 			if(!isset($this->_AsteroidActionID) && isset($_GET['AsteroidActionID']))
@@ -14,19 +17,12 @@
 
 			$this->_AsteroidID = $id;
 			$this->_comets[$id] = array();
-			
-			//Register Scripts If we are not rendering a comet
-			if(!isset($_GET['AsteroidActionID']))
-			{
-				//Script AsteroidJS & JQUERY
-				$cs=Yii::app()->clientScript;
-				$cs->registerCoreScript('jquery');
-				$cs->registerScriptFile($this->getAssetsUrl().'/js/Asteroid.js');
-			}
 
 			return $this;
 		}
-
+		
+		//This method will render all JS and CSS dependencies
+		//You must Call `orbit()` as the very last step after all commets have been initialized with Asteroid('id');
 		public function orbit()
 		{
 			if(!isset($_GET['AsteroidActionID']) && !empty($this->_comets))
@@ -34,36 +30,56 @@
 				$comets = array();
 				foreach($this->_comets as $id=>$config)
 					$comets[] = array('id'=>$id, 'renderType'=>$config->renderType, 'element'=>$config->element);
-				Yii::app()->clientScript->registerCss('asteroidCSS', ".asteroidLoader{background-image:url('".$this->getAssetsUrl().'/images/loading.gif'. "'); background-position:center center; background-repeat:no-repeat; }" , CClientScript::POS_HEAD);
-				Yii::app()->clientScript->registerScript('script', 'var asteroidConfig = ' . CJSON::encode($comets), CClientScript::POS_HEAD);
+				
+				$cs = Yii::app()->clientScript;
+				$cs->registerCoreScript('jquery');
+				$cs->registerCss('asteroidCSS', ".asteroidLoader{background-image:url('".$this->getAssetsUrl().'/images/loading.gif'. "'); background-position:center center; background-repeat:no-repeat; }" , CClientScript::POS_HEAD);
+				$cs->registerScript('script', 'var asteroidConfig = ' . CJSON::encode($comets), CClientScript::POS_HEAD);
+				$cs->registerScriptFile($this->getAssetsUrl().'/js/Asteroid.js');
 			}
 		}
 		
-
+		//Setter for _AsteroidCometRender the sets the Yii render type for your comet
+		//renderPartial is the default. Generaly you need to use pass 'render' if you are using Yii widgets like Grid View.
+		//Passing render will make sure that all scripts that are registered to POS_HEAD are included.
 		public function renderMethod($type='renderPartial')
 		{
 			$this->_AsteroidCometRender = 'render';
 			return $this;
 		}
 
-		public function append($element, $template, $data)
+		//Tells JS to append the content to the dom element :$element 
+		//using the Yii template view: $template
+		//with the data: $data. $data must be a closure that returns an associative array.
+		//String $elment, String $template, Closure $data
+		public function append($element, $template, closure $data)
 		{
 			$this->setComet($this->_AsteroidID, array('renderType'=>'append', 'element'=>$element, 'template'=>$template, 'data'=>$data));
 			return $this;
 		}
 
-		public function prepend($element, $template, $data)
+		//Tells JS to prepend the content to the dom element :$element 
+		//using the Yii template view: $template
+		//with the data: $data. $data must be a closure the returns an array.
+		//String $elment, String $template, Closure $data
+		public function prepend($element, $template, closure $data)
 		{
 			$this->setComet($this->_AsteroidID, array('renderType'=>'prepend', 'element'=>$element, 'template'=>$template, 'data'=>$data));
 			return $this;
 		}
-
-		public function replace($element, $template, $data)
+		
+		//Tells JS to replace the content to the dom element :$element 
+		//using the Yii template view: $template
+		//with the data: $data. $data must be a closure the returns an array.
+		//String $elment, String $template, Closure $data
+		public function replace($element, $template, closure $data)
 		{
 			$this->setComet($this->_AsteroidID, array('renderType'=>'replace', 'element'=>$element, 'template'=>$template, 'data'=>$data));
 			return $this;
 		}
 
+		//This method is called internally by EAsteroid.
+		//Sets the _comets var so that it contains a list of all comets.
 		public function setComet($id, $config = array())
 		{
 			$this->_comets[$id] = (object) $config;
@@ -71,7 +87,9 @@
 				$this->execComet($id);
 			return true;
 		}
-
+		
+		//Internal comet render method.
+		//Renders comet with $id and exits Yii
 		public function execComet($id)
 		{
 			$comet = $this->_comets[$id];
@@ -85,6 +103,7 @@
 			Yii::app()->end();
 		}
 
+		//Pulishes assets and set $this->_assetsUrl to the location of all registered assets
 		public function getAssetsUrl()
     {
 			if ($this->_assetsUrl !== null)
@@ -92,12 +111,10 @@
 			else
 			{
 					$assetsPath = Yii::getPathOfAlias('ext.Asteroid.assets');
-
 					if (YII_DEBUG)
 							$assetsUrl = Yii::app()->assetManager->publish($assetsPath, false, -1, true);
 					else
 							$assetsUrl = Yii::app()->assetManager->publish($assetsPath);
-
 					return $this->_assetsUrl = $assetsUrl;
 			}
     }
